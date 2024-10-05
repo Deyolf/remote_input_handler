8bc2c049c7fa787e030068c70f8379959871e088
Deyolf <colombofabio06@gmail.com>
2024-10-05 16:56:23 +0200

    update volume

--- node_client/static/ScriptHome.js
+++ node_client/static/ScriptHome.js
@@ -17,6 +17,20 @@ function sendKeycap(key) {
     });
 }
 
-function updateValue(){
-    
+function updateVolume(volume){
+    fetch('http://192.168.178.89:50000/receive_volume', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ keycap: key }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("response").innerText = data.message || data.error;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        document.getElementById("response").innerText = "Error sending the keycap!";
+    });
 }
--- py_api/main.py
+++ py_api/main.py
@@ -28,5 +28,30 @@ def receive_keycap():
     else:
         return jsonify({"error": "Invalid input, expected JSON data"}), 400
 
+@app.route('/receive_volume', methods=['POST'])
+def receive_volume():
+    # Check if the request contains JSON data
+    if request.is_json:
+        data = request.get_json()
+        
+        # Retrieve keycap information
+        volume = data.get('volume')
+        
+        if volume:
+            # Here, you can handle the volume, e.g., log it or respond with a message.
+            print(volume)
+            devices = AudioUtilities.GetSpeakers()
+            interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+            volumesys = cast(interface, POINTER(IAudioEndpointVolume))
+
+            # Set volumesys to volume
+            volumesys.SetMasterVolumeLevelScalar((volume/100), None)
+
+            return jsonify({"message": f"Received volume: {volume}"})
+        else:
+            return jsonify({"error": "volume data missing"}), 400
+    else:
+        return jsonify({"error": "Invalid input, expected JSON data"}), 400
+
 if __name__ == '__main__':
     app.run(host='192.168.178.89', port=50000)
