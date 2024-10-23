f46ced7f3d037c643296c4229206e2802d862353
Deyolf <colombofabio06@gmail.com>
2024-10-23 11:08:01 +0200

    Automation

---
+++ Install.bat
@@ -0,0 +1,11 @@
+cd ./node_client/
+npm install
+cd ../py_api/
+pip install flask
+pip install flask_cors
+pip install pyautogui
+pip install socket
+pip install pycaw
+pip install ctypes
+pip install comtypes
+pip install subprocess
--- Launch.bat
+++ Launch.bat
@@ -1,12 +1,2 @@
-cd ./node_client/
-pip install
 cd ../py_api/
-pip install flask
-pip install flask_cors
-pip install pyautogui
-pip install socket
-pip install pycaw
-pip install ctypes
-pip install comtypes
-pip install subprocess
 python main.py
